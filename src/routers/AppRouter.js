3837b335f6e319c59acdc7909884762b1f658cbe
nigeymc <mckenna.niall@gmail.com>
2021-03-01 17:20:06 +0000

    added a 404 component and page to router

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import createHistory from 'history/createBrowserHistory';
 import SideNav from '../components/SideNav';
 import HomePage from '../components/HomePage';
@@ -12,6 +12,7 @@ import JobPageJet2 from "../components/JobPageJet2";
 import MySkillsPage from "../components/MySkillsPage";
 import MyProjectsPage from "../components/MyProjects";
 import ContactMePage from "../components/ContactMe";
+import NotFoundPage from "../components/NotFoundPage";
 
 export const history = createHistory();
 
@@ -19,16 +20,20 @@ const AppRouter = () => (
   <BrowserRouter>
     <div>
       <SideNav />
-      <Route path="/" component={HomePage} exact />
-      <Route path="/about-me" component={AboutPage} />
-      <Route path="/my-experience" exact component={MyExperiencePage} />
-      <Route path="/my-experience/river-island" component={JobPageRiverIsland} />
-      <Route path="/my-experience/stella-and-dot" component={JobPageStellaDot} />
-      <Route path="/my-experience/net-a-porter" component={JobPageNetAPorter} />
-      <Route path="/my-experience/jet2" component={JobPageJet2} />
-      <Route path="/my-skills" component={MySkillsPage} />
-      <Route path="/my-projects" component={MyProjectsPage} />
-      <Route path="/contact-me" component={ContactMePage} />
+      <Switch>
+        <Route path="/" component={HomePage} exact />
+        <Route path="/about-me" component={AboutPage} />
+        <Route path="/my-experience" exact component={MyExperiencePage} />
+        <Route path="/my-experience/river-island" component={JobPageRiverIsland} />
+        <Route path="/my-experience/stella-and-dot" component={JobPageStellaDot} />
+        <Route path="/my-experience/net-a-porter" component={JobPageNetAPorter} />
+        <Route path="/my-experience/jet2" component={JobPageJet2} />
+        <Route path="/my-skills" component={MySkillsPage} />
+        <Route path="/my-projects" component={MyProjectsPage} />
+        <Route path="/contact-me" component={ContactMePage} />
+        <Route component={NotFoundPage} />
+      </Switch>
+
     </div>
   </BrowserRouter>
 );
